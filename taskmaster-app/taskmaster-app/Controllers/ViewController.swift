cda0cad4c2c1882e0b339f0280bc6e18467d5f48
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-08 04:07:45 -0700

    feat: initialize UITableView and implement automatic data reloading

--- taskmaster-app/taskmaster-app/Controllers/ViewController.swift
+++ taskmaster-app/taskmaster-app/Controllers/ViewController.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tb = UITableView()
+        tb.register(UITableViewCell.self, forCellReuseIdentifier: "TodoCell")
+        tb.translatesAutoresizingMaskIntoConstraints = false
+        return tb
+    }()
 
-    var tasks = [TodoTask]()
+    var tasks = [TodoTask]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        
+        tableView.dataSource = self
+        loadData()
         
     }
    
