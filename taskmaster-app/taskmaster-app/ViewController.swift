809c864d61c78034156d8673179db2794b1cf47f
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-07 22:33:26 -0800

    chore: initial xcode project launch

---
+++ taskmaster-app/taskmaster-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskmaster-app
+//
+//  Created by Abdulkarim Mziya on 2026-03-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
