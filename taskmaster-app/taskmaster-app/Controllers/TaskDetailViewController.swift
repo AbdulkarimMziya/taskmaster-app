1944fb2b3814c9c7edb584a3400a75b1fce07a60
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-10 17:47:59 -0700

    feat: implement UI components and constraints for TaskDetailViewController

--- taskmaster-app/taskmaster-app/Controllers/TaskDetailViewController.swift
+++ taskmaster-app/taskmaster-app/Controllers/TaskDetailViewController.swift
@@ -15,6 +15,31 @@ class TaskDetailViewController: UIViewController {
     }
         
     let mode: Mode
+    
+    // UI Components
+    private let titleTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter task name..."
+        tf.font = .systemFont(ofSize: 24, weight: .bold)
+        tf.borderStyle = .none
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Completed"
+        label.font = .systemFont(ofSize: 18, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let statusSwitch: UISwitch = {
+        let sw = UISwitch()
+        sw.onTintColor = .systemGreen
+        sw.translatesAutoresizingMaskIntoConstraints = false
+        return sw
+    }()
         
     init(mode: Mode) {
         self.mode = mode
@@ -27,9 +52,40 @@ class TaskDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray
-
+        view.backgroundColor = .secondarySystemBackground
+        setupUI()
+        configureMode()
+    }
+    
+    private func setupUI() {
         setupNavBarButtons()
+        view.addSubview(titleTextField)
+        view.addSubview(statusLabel)
+        view.addSubview(statusSwitch)
+        
+        NSLayoutConstraint.activate([
+            titleTextField.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleTextField.leadingAnchor.constraint(equalTo:view.leadingAnchor, constant: 20),
+            titleTextField.trailingAnchor.constraint(equalTo:view.trailingAnchor, constant: -20),
+            
+            statusLabel.topAnchor.constraint(equalTo:titleTextField.bottomAnchor, constant: 30),
+            statusLabel.leadingAnchor.constraint(equalTo:view.leadingAnchor, constant: 20),
+            
+            statusSwitch.centerYAnchor.constraint(equalTo:statusLabel.centerYAnchor),
+            statusSwitch.trailingAnchor.constraint(equalTo:view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    private func configureMode() {
+        switch mode {
+        case .add:
+            title = "New Task"
+            statusSwitch.isOn = false
+        case .edit(let task):
+            title = "Edit Task"
+            titleTextField.text = task.title
+            statusSwitch.isOn = task.isCompleted
+        }
     }
     
     private func setupNavBarButtons() {
@@ -41,15 +97,14 @@ class TaskDetailViewController: UIViewController {
             barButtonSystemItem: .done, target: self, action: #selector(didTapDone))
     }
     
-    @objc
-    private func didTapCancel() {
+    @objc private func didTapCancel() {
+        
         dismiss(animated: true)
     }
     
-    @objc
-    private func didTapDone() {
+    @objc private func didTapDone() {
+        // TODO: Logic to call your TaskAPIService will go here
         dismiss(animated: true)
     }
-    
-
 }
+
